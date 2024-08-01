5949cf3057136e8d132f2601e6c1fc662cebb3d0
narmyen <tipananchakreerat@gmail.com>
2024-08-01 23:56:55 +0700

    "class component"

--- course-review-backend/src/app.controller.ts
+++ course-review-backend/src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { deflate } from 'zlib';
 
 @Controller()
 export class AppController {
+  // บอกว่าใช้ service อะไร
   constructor(private readonly appService: AppService) {}
 
   @Get()
---
+++ course-review-backend/src/courses.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+
+  @Get()
+  findAll(): any {
+    return {
+      message: "good morning"
+    };
+  }
+}
